68d9f89e7c2ca81bfd58c5f87b3e65684a47fc45
Zhihui Zhang <zhhzhang@CNzhhzhangP.local>
2018-05-13 11:02:04 +0800

    Prime Factors

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class PrimeFactor {
+    public static void main(String args[]){
+        System.out.println(primeConcrete(30));
+    }
+
+    public static ArrayList<Integer> primeConcrete(int n){
+        ArrayList<Integer> result = new ArrayList<>();
+        int current =  n;
+        for(int i=2;i<n;i++){
+            int temp = current%i;
+            if(temp==0){
+                result.add(i);
+            }
+            current = n/i;
+        }
+        return result;
+    }
 }
